958638afe31f06271a1bdcfba43b8a504f0afce7
Menaa153 <cimbacuan@unal.edu.co>
2025-02-12 12:50:52 -0500

    siu

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -21,9 +21,9 @@ def load_estimator():
     import os
     import pickle
 
-    if not os.path.exists("homework/estimator.pickle"):
+    if not os.path.exists("./homework/estimator.pickle"):
         return None
-    with open("homework/estimator.pickle", "rb") as file:
+    with open("./homework/estimator.pickle", "rb") as file:
         estimator = pickle.load(file)
 
     return estimator
@@ -31,12 +31,12 @@ def load_estimator():
 
 def test_01():
 
-    from sklearn.metrics import accuracy_score
+    from sklearn.metrics import mean_squared_error
 
     x, y = load_data()
     estimator = load_estimator()
 
-    accuracy = accuracy_score(
+    accuracy = mean_squared_error(
         y_true=y,
         y_pred=estimator.predict(x),
     )
